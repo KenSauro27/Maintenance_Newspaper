a31e8de0a44cc8da5a8ca2a9fa836db7738d2d9d
KenSauro27 <167883474+KenSauro27@users.noreply.github.com>
2025-09-20 04:42:36 -0700

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #003366;">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Admin Panel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="articles_from_students.php">Pending Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="articles_submitted.php">My Articles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="edit_requests.php">Edit Requests</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-light" href="core/handleForms.php?logoutUserBtn=1">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
